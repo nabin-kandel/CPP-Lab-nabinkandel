b6d22fce94d6b2c368dfd6f98be8215c3db31d1e
Nabin Kandel <105159506+nabin-kandel@users.noreply.github.com>
2022-11-13 19:14:04 +0545

    Add files via upload

---
+++ LAB1/Area-OOP-CPP.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+    int Length,Breadth;
+    public:
+        void setData(int l,int b)
+        {
+            Length=l;
+            Breadth=b;
+        }
+
+        int getArea()
+        {
+            return(Length*Breadth);
+        }
+};
+
+int main()
+{
+    Rectangle r1;
+    int l,b,area;
+    cout<<"Enter the Length and breadth For Rectangle :";
+    cin>>l>>b;
+    r1.setData(l,b);
+    area=r1.getArea();
+    cout<<"\n Area of the Rectangle="<<area;
+}
